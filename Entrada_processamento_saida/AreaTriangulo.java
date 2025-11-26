ee7701270b7f5228d05fb06f3b5b7260824af184
vinicius0gp <viniciusgabrielpires80@gmail.com>
2025-11-25 23:16:35 -0300

    Update AreaTriangulo.java

--- Entrada_processamento_saida/AreaTriangulo.java
+++ Entrada_processamento_saida/AreaTriangulo.java
@@ -1,2 +1,21 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+package prjAula002;
+import java.util.Scanner;
+
+public class AreaTriangulo {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		double base, altura, area;
+		
+		System.out.println("Digite o valor da base:");
+		base = ler.nextDouble();
+		
+		System.out.println("Digite o valor da altura:");
+		altura = ler.nextDouble();
+		
+		area = base*altura/2;
+		
+		System.out.println("A área do triângulo é de "+area);
+	}
+	
+	
+}
