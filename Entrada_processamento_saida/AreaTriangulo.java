5ecee95698a8a84be1cad8e32a91703467e21953
vinicius0gp <viniciusgabrielpires80@gmail.com>
2025-11-25 23:17:42 -0300

    Update AreaTriangulo.java

--- Entrada_processamento_saida/AreaTriangulo.java
+++ Entrada_processamento_saida/AreaTriangulo.java
@@ -1,21 +1,14 @@
-package prjAula002;
-import java.util.Scanner;
 
 public class AreaTriangulo {
+
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		double base, altura, area;
+		int base, altura, area;
+
+		base = 32;
+		altura = 49;
 		
-		System.out.println("Digite o valor da base:");
-		base = ler.nextDouble();
-		
-		System.out.println("Digite o valor da altura:");
-		altura = ler.nextDouble();
-		
-		area = base*altura/2;
-		
-		System.out.println("A área do triângulo é de "+area);
+		area = (base*altura)/2;
+		System.out.println("Área: "+ area);
 	}
-	
-	
+
 }
